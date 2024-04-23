e0ec50ae53802a46967e12ad2237767c4cd6698f
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2024-04-23 19:52:20 +0300

    Data processing

---
+++ data/FeathersImageDataModule.py
@@ -0,0 +1,102 @@
+import torch
+import lightning as L
+from torch.utils.data import random_split, DataLoader
+# Note - you must have torchvision installed for this example
+from FeathersImageDataset import FeathersImageDataset
+from torch.utils.data import WeightedRandomSampler
+from torchvision.transforms import transforms
+
+
+class FeathersImageDataModule(L.LightningDataModule):
+    def __init__(self, 
+                 train_data_dir: str = "./",
+                 train_data_file: str = "./feathers_data_normalized.csv",
+                 test_data_dir: str | None = None,
+                 test_data_file: str | None = None,
+                 split_ratio: float = 0.2,
+                 img_width: int = 48,
+                 img_height: int = 240,
+                 batch_size: int = 32):
+        super().__init__()
+
+        self.batch_size = batch_size
+
+        self.train_data_dir = train_data_dir
+        self.train_data_file = train_data_file
+
+        self.test_data_dir = test_data_dir if test_data_dir else self.train_data_dir
+        self.test_data_file = test_data_file if test_data_file else self.train_data_file
+
+        self.split_ratio = split_ratio
+
+        self.train_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(size=(img_width, img_height)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomRotation(degrees=15),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+        self.test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(size=(img_width, img_height)),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+
+        self.feathers_train = None
+        self.feathers_test = None
+        self.feathers_val = None
+        self.feathers_predict = None
+
+    def setup(self, stage: str):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit":
+            feathers_full = FeathersImageDataset(self.train_data_file,
+                                                 self.train_data_dir,
+                                                 transform=self.train_transform)
+            data_count = len(feathers_full)
+            validation_count = int(data_count * self.split_ratio)
+
+            self.feathers_train, self.feathers_val = random_split(
+                feathers_full, 
+                [data_count - validation_count, validation_count], 
+                generator=torch.Generator()
+            )
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test":
+            self.feathers_test = FeathersImageDataset(self.test_data_file,
+                                                      self.test_data_dir,
+                                                      transform=self.test_transform)
+
+        if stage == "predict":
+            self.feathers_predict = FeathersImageDataset(self.train_data_file,
+                                                         self.train_data_dir,
+                                                         transform=self.test_transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.feathers_train, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.feathers_val, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.feathers_test, batch_size=self.batch_size)
+
+    def predict_dataloader(self):
+        return DataLoader(self.feathers_predict, batch_size=self.batch_size)
+    
+
+if __name__ == "__main__":
+    dm = FeathersImageDataModule("./dataset/images", "./dataset/data/feathers_data.csv")
+    dm.setup("fit")
+    
+    dloader = dm.train_dataloader()
+
+    clss = dict()
+
+    for batch_index, (batch, label) in enumerate(dloader):
+        for el in label:
+            clss[int(el)] = clss.get(int(el), 0) + 1
+
+    print(clss)
---
+++ data/FeathersImageDataset.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
+from matplotlib import pyplot as plt
+import cv2
+
+
+class FeathersImageDataset(Dataset):
+    def __init__(self, 
+                 annotations_file, 
+                 img_dir, 
+                 transform=None, 
+                 target_transform=None,
+                 ):
+        self.data = pd.read_csv(annotations_file)
+        self.data['species'] = pd.factorize(self.data['species'])[0]
+        self.img_dir = img_dir
+
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        filename = self.data['filename'][idx]
+        filename_parts = filename.split("_")
+
+        img_path = os.path.join(self.img_dir, filename_parts[0], "_".join(filename_parts[1:-1]), filename)
+
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        w, h, _ = image.shape
+        if w > h:
+            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+
+        label = torch.Tensor([self.data['species'][idx]])
+
+        if self.transform:
+            image_tensor = self.transform(image)
+
+        else:
+            image_tensor = torch.from_numpy(image)
+
+
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image_tensor.float(), label.float()
+
+
+if __name__ == "__main__":
+    
+    transform_a = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(size=(48, 240)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.RandomRotation(degrees=15),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    
+    dataset = FeathersImageDataset("./dataset/data/feathers_data.csv",
+                                   "./dataset/images",
+                                   transform=transform_a)
+    data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+    
+    c = 0
+    for imgs, labels in data_loader:
+        print(f"Batch {c} of images has shape: ",imgs.shape)
+        print(f"Batch {c} of labels has shape: ", labels.shape)
